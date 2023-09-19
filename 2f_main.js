5830583d166ca9f9f1c2a9d774bcf9e7d2fc3595
Szebeni Zita <mazsola.muvek@gmail.com>
2023-09-19 03:54:53 +0200

    1. és 2. feladat

---
+++ 2f_main.js
@@ -0,0 +1,32 @@
+function zold() {
+    document.getElementById("elso");
+    elso.style.color = "green";
+
+}
+
+function bevitelBlur() {
+    const input = document.getElementById("elso");
+    input.style.color = "black"; 
+} 
+
+function zold() {
+    document.getElementById("mas");
+    elso.style.color = "green";
+
+}
+
+function bevitelBlur() {
+    const input = document.getElementById("mas");
+    input.style.color = "black"; 
+} 
+
+function zold() {
+    document.getElementById("har");
+    elso.style.color = "green";
+
+}
+
+function bevitelBlur() {
+    const input = document.getElementById("har");
+    input.style.color = "black"; 
+}
